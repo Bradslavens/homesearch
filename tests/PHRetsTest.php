9430da2b6fbd15bc68d82e8f83861135e1882354
vagrant <vagrant@homestead>
2017-06-14 08:06:57 +0000

    phrets images object

--- tests/PHRetsTest.php
+++ tests/PHRetsTest.php
@@ -36,9 +36,10 @@ class PHRetsTest extends TestCase
 
         $classes = $rets->GetClassesMetadata('Property');
         // var_dump($classes->first());
-
-        $objects = $rets->GetObject('Property', 'Photo', '00-1669', '*', 1);
+        
+        // $objects = $rets->GetObject('Property', 'Photo', $r['L_ListingID'], '*', 1);
         // var_dump($objects);
+
 
         $fields = $rets->GetTableMetadata('Property', 'RE_1');
 
@@ -48,7 +49,7 @@ class PHRetsTest extends TestCase
     {
 
         $config = new \PHRETS\Configuration;
-        $config->setLoginUrl('https://rets-paragon.sandicor.com/rets/fnisrets.aspx/SANDICOR/login?rets-version=RETS/1.7.2')
+        $config->setLoginUrl(env('RETS_URL'))
                 ->setUsername(env('RETS_USER'))
                 ->setPassword(env('RETS_PASS'))
                 ->setRetsVersion('1.7.2');
@@ -59,11 +60,27 @@ class PHRetsTest extends TestCase
 
         $connect = $rets->Login();
 
-        $results = $rets->Search('Property', 'RE_1', '(ListPrice=5000000+)', ['Limit' => 3, 'Select' => 'L_AddressStreet']);
+        $results = $rets->Search('Property', 'RE_1', '(ListPrice=5000000+)', ['Limit' => 1, 'Select' => 'L_ListingID']);
         
         foreach ($results as $r) 
         {
-            var_dump($r['L_AddressStreet']);
+            $objects = $rets->GetObject('Property', 'Photo', $r['L_ListingID'], '*',  1);
+
+            $this->assertContains(".JPG", $objects[0]->getLocation());
+
+            // foreach ($objects as $object) 
+            // {
+            //     if(env('APP_ENV') == 'testing')
+            //     {
+            //         $url = preg_replace("/stageimage./", "IMG-", $object->getLocation());
+            //         var_dump($url); // FOR TESTING ONLY
+            //     }
+            //     else
+            //     {
+            //         var_dump($object->getLocation());
+            //     }
+
+            // }
         }
     }
 }
