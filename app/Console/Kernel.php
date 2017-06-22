57f0bc766f19cc678f823d04da3f2f4660572fb9
vagrant <vagrant@homestead>
2017-06-22 00:35:27 +0000

    cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 use Illuminate\Support\Facades\Log;
 use troydavisson\phrets;
+use Carbon\Carbon;
 
 class Kernel extends ConsoleKernel
 {
@@ -29,6 +30,11 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function(){
 
+            $time = Carbon::yesterday()->toAtomString();
+
+            // $query = "(L_UpdateDate=". $time . "+)";
+            $query = "(L_StatusCatID=1)";
+
             // connect to RETS
             $config = new \PHRETS\Configuration;
             $config->setLoginUrl(env('RETS_URL'))
@@ -46,14 +52,33 @@ class Kernel extends ConsoleKernel
             
             Log::info('started query');
             
-            $results = $rets->Search('Property', 'RE_1', '(ListPrice=5000000+)', ['Limit' => 100, 'Select' => ['L_ListingID', 'L_AskingPrice', 'L_AddressNumber', 'L_AddressDirection', 'L_AddressStreet', 'L_Address2', 'L_City', 'L_State', 'L_Zip']]);
+            $results = $rets->Search('Property', 'RE_1', $query, ['Limit' => 1000, 'select' => ['L_ListingID', 'L_AskingPrice', 'L_AddressNumber', 'L_AddressDirection', 'L_AddressStreet', 'L_Address2', 'L_City', 'L_State', 'L_Zip']]);
             
             log::info('ended query');
 
             foreach ($results as $r) 
             {
-                // add the property to the database
-                $property = \App\Property::create(['L_ListingID' => $r['L_ListingID'], 'L_AskingPrice' => $r['L_AskingPrice'], 'L_AddressNumber' => $r['L_AddressNumber'], 'L_AddressDirection' => $r['L_AddressDirection'], 'L_AddressStreet' => $r['L_AddressStreet'], 'L_Address2' => $r['L_Address2'], 'L_City' => $r['L_City'], 'L_State' => $r['L_State'], 'L_Zip' => $r['L_Zip']]);
+                // first see if the listing exits
+                
+                $property = \App\Property::where('L_ListingID', $r['L_ListingID'])->first();
+
+                Log::info('property count ' .$property->count()); 
+
+                if($property->count() == 0)
+                {
+                    Log::info('property does not exist');
+
+                    // add the property to the database
+                    \App\Property::create(['L_ListingID' => $r['L_ListingID'], 'FullAddress' => $r['L_AddressNumber'] . " " . $r['L_AddressDirection'] . " " . $r['L_AddressStreet'] . ", " . $r['L_City']. ", " . $r['L_State'] . " " . $r['L_Zip']  , 'L_AskingPrice' => $r['L_AskingPrice'], 'L_AddressNumber' => $r['L_AddressNumber'], 'L_AddressDirection' => $r['L_AddressDirection'], 'L_AddressStreet' => $r['L_AddressStreet'], 'L_Address2' => $r['L_Address2'], 'L_City' => $r['L_City'], 'L_State' => $r['L_State'], 'L_Zip' => $r['L_Zip']]);
+                }
+                else
+                {
+
+                    Log::info('property already exists');
+
+                    \App\Property::find($property->id)->update(['L_ListingID' => $r['L_ListingID'], 'FullAddress' => $r['L_AddressNumber'] . " " . $r['L_AddressDirection'] . " " . $r['L_AddressStreet'] . ", " . $r['L_City']. ", " . $r['L_State'] . " " . $r['L_Zip']  , 'L_AskingPrice' => $r['L_AskingPrice'], 'L_AddressNumber' => $r['L_AddressNumber'], 'L_AddressDirection' => $r['L_AddressDirection'], 'L_AddressStreet' => $r['L_AddressStreet'], 'L_Address2' => $r['L_Address2'], 'L_City' => $r['L_City'], 'L_State' => $r['L_State'], 'L_Zip' => $r['L_Zip']]);
+                }
+
 
                 // add the property images to the images database
                 $objects = $rets->GetObject('Property', 'Photo', $r['L_ListingID'], '*',  1);
