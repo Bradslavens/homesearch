e5d2570de93d8a4356c0894355c5f7afea7aa6f0
bradslavens <bradslavens@gmail.com>
2017-06-28 05:41:15 +0000

    made property fields nullable

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -52,7 +52,7 @@ class Kernel extends ConsoleKernel
             
             Log::info('started query');
             
-            $results = $rets->Search('Property', 'RE_1', $query, ['Limit' => 1, 'select' => ['L_ListingID', 'L_AskingPrice', 'L_AddressNumber', 'L_AddressDirection', 'L_AddressStreet', 'L_Address2', 'L_City', 'L_State', 'L_Zip', 'LM_Int1_3','LM_Int2_3','LM_Int1_5','LM_Int4_1',]]);
+            $results = $rets->Search('Property', 'RE_1', $query, ['Limit' => 1, 'select' => ['L_ListingID', 'L_AskingPrice', 'L_AddressNumber', 'L_AddressDirection', 'L_AddressStreet', 'L_Address2', 'L_City', 'L_State', 'L_Zip', 'LM_Int1_3','LM_Int2_3','LM_Int1_5','LM_Int4_1','L_UpdateDate', 'L_ListingDate',]]);
             
             log::info('ended query');
 
@@ -67,14 +67,14 @@ class Kernel extends ConsoleKernel
                     Log::info('property does not exist');
 
                     // add the property to the database
-                    $property = \App\Property::create(['L_ListingID' => $r['L_ListingID'], 'FullAddress' => $r['L_AddressNumber'] . " " . $r['L_AddressDirection'] . " " . $r['L_AddressStreet'] . ", " . $r['L_City']. ", " . $r['L_State'] . " " . $r['L_Zip']  , 'L_AskingPrice' => $r['L_AskingPrice'], 'L_AddressNumber' => $r['L_AddressNumber'], 'L_AddressDirection' => $r['L_AddressDirection'], 'L_AddressStreet' => $r['L_AddressStreet'], 'L_Address2' => $r['L_Address2'], 'L_City' => $r['L_City'], 'L_State' => $r['L_State'], 'L_Zip' => $r['L_Zip'], 'LM_Int1_3' => $r['LM_Int1_3'],'LM_Int2_3' => $r['LM_Int2_3'],'LM_Int1_5' => $r['LM_Int1_5'],'LM_Int4_1' => $r['LM_Int4_1'],]);
+                    $property = \App\Property::create(['L_ListingID' => $r['L_ListingID'], 'FullAddress' => $r['L_AddressNumber'] . " " . $r['L_AddressDirection'] . " " . $r['L_AddressStreet'] . ", " . $r['L_City']. ", " . $r['L_State'] . " " . $r['L_Zip']  , 'L_AskingPrice' => $r['L_AskingPrice'], 'L_AddressNumber' => $r['L_AddressNumber'], 'L_AddressDirection' => $r['L_AddressDirection'], 'L_AddressStreet' => $r['L_AddressStreet'], 'L_Address2' => $r['L_Address2'], 'L_City' => $r['L_City'], 'L_State' => $r['L_State'], 'L_Zip' => $r['L_Zip'], 'LM_Int1_3' => $r['LM_Int1_3'],'LM_Int2_3' => $r['LM_Int2_3'],'LM_Int1_5' => $r['LM_Int1_5'],'LM_Int4_1' => $r['LM_Int4_1'], 'L_UpdateDate' => $r['L_UpdateDate'], 'L_ListingDate' => $r['L_ListingDate'],]);
                 }
                 else
                 {
 
                     Log::info('property already exists');
 
-                    \App\Property::find($property->id)->update(['L_ListingID' => $r['L_ListingID'], 'FullAddress' => $r['L_AddressNumber'] . " " . $r['L_AddressDirection'] . " " . $r['L_AddressStreet'] . ", " . $r['L_City']. ", " . $r['L_State'] . " " . $r['L_Zip']  , 'L_AskingPrice' => $r['L_AskingPrice'], 'L_AddressNumber' => $r['L_AddressNumber'], 'L_AddressDirection' => $r['L_AddressDirection'], 'L_AddressStreet' => $r['L_AddressStreet'], 'L_Address2' => $r['L_Address2'], 'L_City' => $r['L_City'], 'L_State' => $r['L_State'], 'LM_Int1_3' => $r['LM_Int1_3'],'LM_Int2_3' => $r['LM_Int2_3'],'LM_Int1_5' => $r['LM_Int1_5'],'LM_Int4_1' => $r['LM_Int4_1'],]);
+                    \App\Property::find($property->id)->update(['L_ListingID' => $r['L_ListingID'], 'FullAddress' => $r['L_AddressNumber'] . " " . $r['L_AddressDirection'] . " " . $r['L_AddressStreet'] . ", " . $r['L_City']. ", " . $r['L_State'] . " " . $r['L_Zip']  , 'L_AskingPrice' => $r['L_AskingPrice'], 'L_AddressNumber' => $r['L_AddressNumber'], 'L_AddressDirection' => $r['L_AddressDirection'], 'L_AddressStreet' => $r['L_AddressStreet'], 'L_Address2' => $r['L_Address2'], 'L_City' => $r['L_City'], 'L_State' => $r['L_State'], 'LM_Int1_3' => $r['LM_Int1_3'],'LM_Int2_3' => $r['LM_Int2_3'],'LM_Int1_5' => $r['LM_Int1_5'],'LM_Int4_1' => $r['LM_Int4_1'], 'L_UpdateDate' => $r['L_UpdateDate'], 'L_ListingDate' => $r['L_ListingDate'],]);
 
                     $property = \App\Property::find($property->id);
                 }
--- database/migrations/2017_06_14_225154_create_properties_table.php
+++ database/migrations/2017_06_14_225154_create_properties_table.php
@@ -16,19 +16,20 @@ class CreatePropertiesTable extends Migration
         Schema::create('properties', function (Blueprint $table) {
             $table->increments('id');
             $table->string('L_ListingID');
-            $table->string('FullAddress');
-            $table->integer('L_AskingPrice');
-            $table->string('L_AddressNumber');
-            $table->string('L_AddressDirection');
-            $table->string('L_AddressStreet');
-            $table->string('L_Address2');
-            $table->string('L_City');
-            $table->string('L_State');
-            $table->string('L_Zip');
-            $table->string('LM_Int1_3'); // total bedrooms
-            $table->string('LM_Int2_3'); // full baths
-            $table->string('LM_Int1_5'); // 1/2 baths
-            $table->string('LM_Int4_1'); // estimated square feet
+            $table->string('FullAddress')->nullable();
+            $table->integer('L_AskingPrice')->nullable();
+            $table->string('L_AddressNumber')->nullable();
+            $table->string('L_AddressDirection')->nullable();
+            $table->string('L_AddressStreet')->nullable();
+            $table->string('L_Address2')->nullable();
+            $table->string('L_City')->nullable();
+            $table->string('L_State')->nullable();
+            $table->string('L_Zip')->nullable();
+            $table->string('LM_Int1_3')->nullable(); // total bedrooms
+            $table->string('LM_Int2_3')->nullable(); // full baths
+            $table->string('LM_Int1_5')->nullable(); // 1/2 baths
+            $table->string('L_UpdateDate'); // updated date
+            $table->string('L_ListingDate'); // list date
             $table->timestamps();
         });
     }
