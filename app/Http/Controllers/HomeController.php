65616a70fa542ff2be0446a76325b319185f02b2
bradslavens <bradslavens@gmail.com>
2017-07-08 22:25:04 +0000

    changed listing count to active

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $listingCount = Property::all()->count();
+        $listingCount = Property::where('L_StatusCatID', 1)->count();
 
         return view('welcome', ['listingCount' => $listingCount]);
     }
