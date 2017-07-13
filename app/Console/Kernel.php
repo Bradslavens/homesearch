f2edc64824c62037159a3c77222a01a6a0d05186
bradslavens <bradslavens@gmail.com>
2017-07-13 02:07:07 +0000

    created filter3

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -55,7 +55,7 @@ class Kernel extends ConsoleKernel
 
             // set limit
             // $limit = 2000;
-            $limit = 1; // for testing
+            $limit = 1; // for testing .
             
                
             $select = [
