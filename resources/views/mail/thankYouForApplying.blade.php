cda87175bf5b174ce26d430f1152eee36a7510db
vagrant <bradslavens@gmail.com>
2017-07-23 20:55:00 +0000

    fixed thank you for applying mail

--- resources/views/mail/thankYouForApplying.blade.php
+++ resources/views/mail/thankYouForApplying.blade.php
@@ -1,21 +1,3 @@
-@extends('layouts.main')
+<p>Dear {{$applicant->name}},</p>
 
-@section('title')
-    <title>Slavens Realty Thank You for Applying!</title>
-@endsection
-
-@section('content')
-    <p>Dear, {{$applicant->name}}, Thank you for applying to Slavens Realty.  We'll get back to you shortly.</p>
-@endsection
-
-@section('nav')
-    @include('partials.nav-inverse')
-@endsection
-
-@section('css')
-    <link rel="stylesheet" type="text/css" href="/css/careers.css">
-@endsection
-
-@section('footer')
-    @include('partials.footer2')
-@endsection
+<p>Thank you for applying to Slavens Realty.  We'll get back to you shortly.</p>
