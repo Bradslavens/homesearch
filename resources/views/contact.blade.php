e064b67091e00dd88cc811586c4760f3427d37d7
vagrant <bradslavens@gmail.com>
2017-07-26 02:10:28 +0000

    contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,7 +6,7 @@
 @endsection
 
 @section('css')
-    <link rel="stylesheet" type="text/css" href="/css/careers.css">
+    <link rel="stylesheet" type="text/css" href="/css/contact.css">
 @endsection
 
 @section('nav')
@@ -33,25 +33,13 @@
         <form id="form" class="job-container" method="POST" action="{{route('contact.store')}}">
             {{ csrf_field() }}
 
-        <div class="form-group">
-            <label for="name">Name</label>
             <input type="text" class="form-control" name="name" id="name" placeholder="Enter Name" required>
-        </div>
 
-        <div class="form-group">
-            <label for="Phone">Phone</label>
             <input type="tel" class="form-control" name="phone" id="phone" placeholder="Phone" >
-        </div>
 
-        <div class="form-group">
-            <label for="email">email</label>
             <input type="email" class="form-control" name="email" id="email" placeholder="email" required>
-        </div>
 
-        <div class="form-group">
-            <label for="comment">Comment</label>
             <textarea class="form-control" name="comment" id="comment" placeholder="comment"></textarea>
-        </div>
 
         <input type="submit" id="submit" value="Send">
 
